b677d5976b3a6671e64e80ce315f62e6b9f3f181
Suryaprkash03 <suryatheking5784@gmail.com>
2025-04-11 12:50:38 +0530

    Add Dockerfile and app.py for Python container setup

---
+++ Assignments/container_python/app.py
@@ -0,0 +1,4 @@
+# This is a simple Python script that prints a message to the console.
+# It is intended to be run inside a Docker container.
+
+print("🔥 Hello from inside a Docker container running Python!")
